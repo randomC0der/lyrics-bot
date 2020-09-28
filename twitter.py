1622745ae84b8d98334ac2d3edf50921ab2e0369
Lukas Hein <lukashein@protonmail.com>
2020-09-28 20:13:58 +0200

    Added Twitter Hello World and error handling

--- twitter.py
+++ twitter.py
@@ -1,13 +1,38 @@
+import os
+import sys
+import logging
 import tweepy
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("todo", "todo")
-auth.set_access_token("todo", "todo")
+from datetime import date
+
+#setup logging
+logpath = "logs"
+if not os.path.exists(logpath):
+    os.mkdir(logpath)
+logging.basicConfig(filename=os.path.join(logpath, str(date.today()) + '.log'), level=logging.INFO)
+logging.info("Program started.")
+
+try:
+    auth = tweepy.OAuthHandler(os.environ["TwitterConsumerKey"], os.environ["TwitterConsumerSecret"])
+    auth.set_access_token(os.environ["TwitterAccessToken"], os.environ["TwitterAccessSecret"])
+
+except KeyError as missing_key:
+    logging.error(f"Environment variable is missing: {missing_key}. Exiting program.")
+    sys.exit(1)
 
 api = tweepy.API(auth)
 
 try:
     api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+    logging.info("Authentication OK")
+
+    api.update_status("Hello, World!")
+
+except tweepy.TweepError as twitter_error:
+    error_json = twitter_error.response.text
+    logging.error(f"Error {error_json.code}: {error_json.message}. Exiting program.")
+    sys.exit(1)
+
+except Exception as ex:
+    logging.error(f"An unknown exception occurred: {ex}. Exiting program.")
+    sys.exit(1)
