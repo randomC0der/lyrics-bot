51452636f432a51d67521f8b2d76a6c1d2ed0515
Lukas Hein <lukashein@protonmail.com>
2020-09-29 14:46:50 +0200

    Print async lyrics

--- twitter.py
+++ twitter.py
@@ -1,38 +1,47 @@
 import os
 import sys
+import asyncio
 import logging
 import tweepy
 
 from datetime import date
 
-#setup logging
-logpath = "logs"
-if not os.path.exists(logpath):
-    os.mkdir(logpath)
-logging.basicConfig(filename=os.path.join(logpath, str(date.today()) + '.log'), level=logging.INFO)
-logging.info("Program started.")
+from core import LyricsBot
 
-try:
-    auth = tweepy.OAuthHandler(os.environ["TwitterConsumerKey"], os.environ["TwitterConsumerSecret"])
-    auth.set_access_token(os.environ["TwitterAccessToken"], os.environ["TwitterAccessSecret"])
+async def main():
+    #setup logging
+    logpath = "logs"
+    if not os.path.exists(logpath):
+        os.mkdir(logpath)
+    logging.basicConfig(filename=os.path.join(logpath, str(date.today()) + '.log'), level=logging.INFO)
+    logging.info("Program started.")
 
-except KeyError as missing_key:
-    logging.error(f"Environment variable is missing: {missing_key}. Exiting program.")
-    sys.exit(1)
+    try:
+        auth = tweepy.OAuthHandler(os.environ["TwitterConsumerKey"], os.environ["TwitterConsumerSecret"])
+        auth.set_access_token(os.environ["TwitterAccessToken"], os.environ["TwitterAccessSecret"])
 
-api = tweepy.API(auth)
+    except KeyError as missing_key:
+        logging.error(f"Environment variable is missing: {missing_key}. Exiting program.")
+        sys.exit(1)
 
-try:
-    api.verify_credentials()
-    logging.info("Authentication OK")
+    api = tweepy.API(auth)
+    bot = LyricsBot()
 
-    api.update_status("Hello, World!")
+    try:
+        api.verify_credentials()
+        logging.info("Authentication OK")
 
-except tweepy.TweepError as twitter_error:
-    error_json = twitter_error.response.text
-    logging.error(f"Error {error_json.code}: {error_json.message}. Exiting program.")
-    sys.exit(1)
+        while True:
+            print(bot.get_lyrics())
+            await asyncio.sleep(60 * 3)
 
-except Exception as ex:
-    logging.error(f"An unknown exception occurred: {ex}. Exiting program.")
-    sys.exit(1)
+    except tweepy.TweepError as twitter_error:
+        error_json = twitter_error.response.text
+        logging.error(f"Error {error_json.code}: {error_json.message}. Exiting program.")
+        sys.exit(1)
+
+    except Exception as ex:
+        logging.error(f"An unknown exception occurred: {ex}. Exiting program.")
+        sys.exit(1)
+
+asyncio.run(main())
