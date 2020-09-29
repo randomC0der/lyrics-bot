c1cf9132b0a74d37c83f49bf8c7f8a379c68e048
Lukas Hein <lukashein@protonmail.com>
2020-09-29 14:47:41 +0200

    Ignore whitespace

--- core.py
+++ core.py
@@ -17,12 +17,23 @@ class LyricsBot:
     def __get_lines(self, lyrics) -> int:
         self.__cache_count += 1
 
+        # todo: genius integration
         if self.__cache_count > self.__cycle:
             self.__cache_count = 0
-            self.__line_count = sum(1 for _ in lyrics)
-            lyrics.seek(0) # to the beginning
+            self.__count_lines(lyrics)
 
         return self.__line_count
+    
+    def __count_lines(self, lyrics):
+        count = 0
+
+        for line in lyrics:
+            # todo: filter genius lines
+            if line and not line.isspace():
+                count += 1
+        lyrics.seek(0) # to the beginning
+
+        self.__line_count = count
 
     def get_lyrics(self) -> str:
         with open(self.__filename, encoding="utf-8") as lyrics:
@@ -30,11 +41,11 @@ class LyricsBot:
             # depending on how many songs there are, the file could turn out quite big so it gets read line by line
             # unfortunately, this means the file could be read two times
             
-            selected = random.randrange(0, self.__get_lines(lyrics))
+            selected = random.randint(0, self.__get_lines(lyrics))
 
             # there is no need to cache the lines
             for i, line in enumerate(lyrics):
                 if i == selected:
-                    return line
+                    return line.rstrip()
         
         raise IndexError
